5cc2272ee1321fd059a62f5eae846939e74a232a
Quique Ciria <quiqueciria@gmail.com>
2025-02-06 10:33:00 +0100

    upadate all

--- sync-strava.js
+++ sync-strava.js
@@ -55,9 +55,11 @@ async function getActivities() {
         (activity.distance / 1000).toFixed(2)
       );
       // Convertir a Horas
-      const elapsedTimeInHours = parseFloat(
-        (activity.elapsed_time / 3600).toFixed(2)
-      );
+      const totalSeconds = elapsedTimeInHours;
+      const hours = Math.floor(totalSeconds / 3600); // 5400 / 3600 = 1 hora
+      const minutes = Math.floor((totalSeconds % 3600) / 60); // 0 minutos
+      const elapsedTimeFormatted = `${hours}h ${minutes}m`;
+
       // Convertir a km/h
       const averageSpeedKmH = parseFloat(
         (activity.average_speed * 3.6).toFixed(2)
@@ -96,7 +98,7 @@ async function getActivities() {
               },
             },
             Elapsed: {
-              number: elapsedTimeInHours,
+              number: elapsedTimeFormatted,
             },
             Media: {
               number: averageSpeedKmH,
